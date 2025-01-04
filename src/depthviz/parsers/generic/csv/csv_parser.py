23ed59d96dbc72a751cc78aa14baf39da4986762
Noppanut Ploywong <noppanut.connect@gmail.com>
2025-01-04 16:14:28 +0700

    refactor: migrate previous CSV parser into Apnealizer class

--- src/depthviz/parsers/generic/csv/csv_parser.py
+++ src/depthviz/parsers/generic/csv/csv_parser.py
@@ -1,8 +1,9 @@
 """
-This module contains the CsvParser class which is used to parse a CSV file containing depth data.
+This module contains the CsvParser base class 
+which is used to parse a CSV file containing depth data.
 """
 
-import csv
+from abc import ABC, abstractmethod
 
 
 class CsvParserError(Exception):
@@ -25,42 +26,25 @@ class EmptyFileError(CsvParserError):
     """Exception raised for empty file errors."""
 
 
-class CsvParser:
+class CsvParser(ABC):
     """
     A class to parse a CSV file containing depth data.
     """
 
-    def __init__(self) -> None:
-        self.depth_data: list[float] = []
-
+    @abstractmethod
     def parse(self, file_path: str) -> None:
         """
         Parses a CSV file containing depth data.
-        Args:
-            file_path: Path to the CSV file containing depth data.
-        """
-        try:
-            with open(file_path, "r", encoding="utf-8") as file:
-                reader = csv.DictReader(file, delimiter=",")
-                for row in reader:
-                    if "Depth" in row:
-                        try:
-                            self.depth_data.append(float(row["Depth"]))
-                        except ValueError as e:
-                            raise InvalidDepthValueError(
-                                "Invalid CSV: Invalid depth values"
-                            ) from e
-                    else:
-                        raise InvalidHeaderError("Invalid CSV: Target header not found")
-            if not self.depth_data:
-                raise EmptyFileError("Invalid CSV: File is empty")
-        except FileNotFoundError as e:
-            raise CsvFileNotFoundError(
-                f"Invalid CSV: File not found: {file_path}"
-            ) from e
 
+        Parameters:
+        file_path (str): The path to the CSV file to be parsed.
+        """
+
+    @abstractmethod
     def get_depth_data(self) -> list[float]:
         """
-        Returns the depth data.
+        Returns the depth data parsed from the CSV file.
+
+        Returns:
+        list[float]: The depth data parsed from the CSV file.
         """
-        return self.depth_data
