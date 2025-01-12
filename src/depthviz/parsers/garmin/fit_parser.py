9600948d87328ffa1044d8f25d796333130232fe
Noppanut Ploywong <noppanut.connect@gmail.com>
2025-01-12 23:58:56 +0700

    draft: Garmin fit testing

--- src/depthviz/parsers/garmin/fit_parser.py
+++ src/depthviz/parsers/garmin/fit_parser.py
@@ -1,1 +1,15 @@
+from garmin_fit_sdk import Decoder, Stream
 
+stream = Stream.from_file("fitfiletools.fit")
+decoder = Decoder(stream)
+messages, errors = decoder.read()
+
+max_depth = 0
+for record in messages.get("record_mesgs", []):
+    max_depth = max(max_depth, record["depth"])
+
+# print(record["timestamp"], record["depth"])
+print(max_depth)
+
+# print(errors)
+# print(messages)
