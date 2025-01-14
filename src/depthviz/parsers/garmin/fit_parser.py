555515a98ca8b14491ed601b2d72166079e6938e
Noppanut Ploywong <noppanut.connect@gmail.com>
2025-01-14 22:23:46 +0700

    feat: add Garmin FIT parser

--- src/depthviz/parsers/garmin/fit_parser.py
+++ src/depthviz/parsers/garmin/fit_parser.py
@@ -1,83 +1,141 @@
-from garmin_fit_sdk import Decoder, Stream
-from pprint import pprint
+"""
+This module contains the GarminFitParser class 
+which is used to parse a Garmin FIT file from Garmin Connect
+"""
+
 import math
+from typing import cast
+from garmin_fit_sdk import Decoder, Stream
 
-stream = Stream.from_file("../../../../tests/data/garmin/14087156326_ACTIVITY.fit")
-decoder = Decoder(stream)
-messages, errors = decoder.read(convert_datetimes_to_dates=False)
-file_type = messages.get("file_id_mesgs")[0].get("type")  # activity
-
-sport = messages.get("sport_mesgs")[0].get(
-    "sport"
-)  # diving ; there is also one in lap_mesgs
-# buttom time here is accurate
-dive_summary = []
-dive_summary_mesgs = messages.get("dive_summary_mesgs", [])
-
-for dive_summary_mesg in dive_summary_mesgs:
-    if dive_summary_mesg.get("reference_mesg") != "lap":
-        continue
-    lap_idx = dive_summary_mesg.get("reference_index")
-    lap_mesg = messages.get("lap_mesgs")[lap_idx]
-    bottom_time = dive_summary_mesg.get("bottom_time")
-    start_time = lap_mesg.get("start_time")
-    end_time = math.floor(start_time + bottom_time)
-    dive_summary.append(
-        {
-            "start_time": start_time,
-            "end_time": end_time,
-            "max_depth": dive_summary_mesg.get("max_depth"),
-            "avg_depth": dive_summary_mesg.get("avg_depth"),
-            "bottom_time": bottom_time,
-        }
-    )
-selected_dive_index = 8
-records = messages.get("record_mesgs", [])
-
-pprint(dive_summary[selected_dive_index])
-# pprint(records)
-
-MARGIN_START_TIME = 2
-first_timestamp = None
-previous_depth = None
-is_dive_end = False
-for record in records:
-    if (
-        record.get("timestamp")
-        < dive_summary[selected_dive_index].get("start_time") - MARGIN_START_TIME
-    ):
-        continue
-    # After the dive ends, get the depth until it starts to increase or is 0
-    if (
-        record.get("timestamp")
-        >= dive_summary[selected_dive_index].get("end_time") - MARGIN_START_TIME
-    ):
-        is_dive_end = True
-    if is_dive_end and record.get("depth") > previous_depth:
-        break
-
-    if first_timestamp is None:
-        first_timestamp = record.get("timestamp")
-
-    time = record.get("timestamp") - first_timestamp
-    depth = record.get("depth")
-    print(time, depth)
-    previous_depth = record.get("depth")
-
-    # When the dive reaches the buttom time, after that when the depth is 0, the dive is considered to be ended
-    if is_dive_end and round(record.get("depth"), 3) == 0:
-        break
+from depthviz.parsers.generic.generic_divelog_parser import DiveLogFileNotFoundError
+from depthviz.parsers.generic.fit.fit_parser import (
+    DiveLogFitParser,
+    DiveLogFitInvalidFitFileError,
+    DiveLogFitInvalidFitFileTypeError,
+    DiveLogFitDiveNotFoundError,
+)
 
 
-# print(messages.keys())
+class GarminFitParser(DiveLogFitParser):
+    """
+    A class to parse a FIT file containing depth data.
+    """
 
+    def __init__(self, selected_dive_idx: int = 0) -> None:
+        self.__time_data: list[float] = []
+        self.__depth_data: list[float] = []
+        self.__margin_start_time = 2
 
-# max_depth = 0
-# for record in messages.get("record_mesgs", []):
-#     max_depth = max(max_depth, record["depth"])
+        # Select the dive to be parsed (in case of multiple dives in FIT file)
+        self.__selected_dive_idx = selected_dive_idx
+        # TODO: Add a check to see if the selected dive index is valid
 
-# print(record["timestamp"], record["depth"])
-# print(max_depth)
+    def parse(self, file_path: str) -> None:
+        """
+        A method to parse a FIT file containing depth data.
+        """
+        try:
+            stream = Stream.from_file(file_path)
+            decoder = Decoder(stream)
+            messages, errors = decoder.read(convert_datetimes_to_dates=False)
+            if errors:
+                raise errors[0]
+        except RuntimeError as e:
+            raise DiveLogFitInvalidFitFileError(f"Invalid FIT file: {file_path}") from e
+        except FileNotFoundError as e:
+            raise DiveLogFileNotFoundError(f"File not found: {file_path}") from e
 
-# print(errors)
-# print(messages)
+        try:
+            file_id_mesgs = messages.get("file_id_mesgs", [])
+            file_type = file_id_mesgs[0].get("type")
+        except (TypeError, IndexError) as e:
+            raise DiveLogFitInvalidFitFileError(f"Invalid FIT file: {file_path}") from e
+
+        if file_type != "activity":
+            raise DiveLogFitInvalidFitFileTypeError(
+                f"Invalid FIT file type: You must import 'activity', not '{file_type}'"
+            )
+
+        dive_summary = []
+        dive_summary_mesgs = messages.get("dive_summary_mesgs", [])
+
+        for dive_summary_mesg in dive_summary_mesgs:
+            if dive_summary_mesg.get("reference_mesg") != "lap":
+                continue
+            lap_idx = dive_summary_mesg.get("reference_index")
+            lap_mesg = messages.get("lap_mesgs")[lap_idx]
+            bottom_time = dive_summary_mesg.get("bottom_time")
+            start_time = lap_mesg.get("start_time")
+            end_time = math.ceil(start_time + bottom_time)
+            dive_summary.append(
+                {
+                    "start_time": start_time,
+                    "end_time": end_time,
+                    "max_depth": dive_summary_mesg.get("max_depth"),
+                    "avg_depth": dive_summary_mesg.get("avg_depth"),
+                    "bottom_time": bottom_time,
+                }
+            )
+
+        if not dive_summary:
+            raise DiveLogFitDiveNotFoundError(
+                f"Invalid FIT file: {file_path} does not contain any dive data"
+            )
+
+        if (
+            self.__selected_dive_idx >= len(dive_summary)
+            or self.__selected_dive_idx < 0
+        ):
+            raise DiveLogFitDiveNotFoundError(
+                f"Invalid Dive Index: {self.__selected_dive_idx} is not a valid dive index"
+            )
+
+        records = messages.get("record_mesgs", [])
+        first_timestamp = None
+
+        for record in records:
+            timestamp_now = cast(int, record.get("timestamp"))
+            start_time = cast(
+                int, dive_summary[self.__selected_dive_idx].get("start_time")
+            )
+            end_time = cast(int, dive_summary[self.__selected_dive_idx].get("end_time"))
+
+            # Skip the records before the dive starts
+            if timestamp_now < start_time - self.__margin_start_time:
+                continue
+            # After the dive ends, stop getting the depth data
+            if timestamp_now > end_time:
+                break
+
+            if first_timestamp is None:
+                first_timestamp = timestamp_now
+
+            time = float(timestamp_now - first_timestamp)
+            depth = cast(float, record.get("depth"))
+            self.__time_data.append(time)
+            self.__depth_data.append(depth)
+
+            # If the depth is 0, the dive is considered to be ended
+            if round(depth, 3) == 0:
+                break
+
+        if not self.__time_data or not self.__depth_data:
+            raise DiveLogFitDiveNotFoundError(
+                f"Invalid Dive Data: Dive data not found in FIT file: {file_path}"
+            )
+
+    def get_time_data(self) -> list[float]:
+        """
+        Returns the time data parsed from the FIT file.
+        Returns:
+            The time data parsed from the FIT file.
+        """
+        return self.__time_data
+
+    def get_depth_data(self) -> list[float]:
+        """
+        Returns the depth data parsed from the FIT file.
+        Returns:
+            The depth data parsed from the FIT file.
+        """
+        return self.__depth_data
