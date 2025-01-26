1043c067cd7215c011db1c52138959972b807c76
Noppanut Ploywong <noppanut.connect@gmail.com>
2025-01-26 19:17:10 +0700

    chore: add standard copyright and license headers

--- src/depthviz/__main__.py
+++ src/depthviz/__main__.py
@@ -1,5 +1,9 @@
+# Copyright (c) 2024 - 2025 Noppanut Ploywong (@noppanut15) <noppanut.connect@gmail.com>
+# Apache License 2.0 (see LICENSE file or http://www.apache.org/licenses/LICENSE-2.0)
+
+
 """
-This is the entry point of the package.
+This module is the main entry point for the command line interface (CLI) of the DepthViz package.
 """
 
 import sys
--- src/depthviz/optimizer/linear_interpolation.py
+++ src/depthviz/optimizer/linear_interpolation.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2024 - 2025 Noppanut Ploywong (@noppanut15) <noppanut.connect@gmail.com>
+# Apache License 2.0 (see LICENSE file or http://www.apache.org/licenses/LICENSE-2.0)
+
+
 """
 This module provides a class to perform linear interpolation on depth data.
 """
